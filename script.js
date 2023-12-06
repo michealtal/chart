7f733c1b7882861dddda801dd1c141a133d10917
michealtal <talabimicheal09@gmail.com>
2023-12-06 03:39:08 -0800

    second capstone project

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const myform = document.querySelectorAll('#form');
+const name = document.querySelectorAll('#name');
+const password= document.querySelectorAll('#password');
+const number= document.querySelectorAll('#number');
+const msg= document.querySelectorAll('.msg');
+
+myform.document.('.sub', onSubmit);
+
+function onSubmit (e) {
+    console.log(name.value)
+}
